757f4cb6f157e902b5d27f7f4285b03213da2148
vivek <vivek.dahal10@gmail.com>
2020-06-30 04:19:04 -0500

    React Basic UI Crud

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component, Fragment } from "react";
+import AppHeader from "./components/AppHeader";
+import AppFooter from "./components/AppFooter";
+import Home from "./components/DataTable/Home";
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <AppHeader />
+        <Home />
+        <AppFooter />
+      </Fragment>
+    );
+  }
+}
+export default App;
